5405dd88987f3a58a116cc24a61f45b5370670b8
Sibgatulin <renat.sibgatulin@gmail.com>
2020-02-04 17:59:09 +0100

    A little nicer grep call

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Launch one instance of primary bar for each connected monitor
-for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+for m in $(xrandr --query | grep -w connected | cut -d" " -f1); do
     MONITOR=$m polybar --reload primary &
 done
 
