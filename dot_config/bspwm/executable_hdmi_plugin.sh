1b09bddc6613ca68d847bf822e28b5320327e85f
Sibgatulin <renat.sibgatulin@gmail.com>
2021-06-02 19:28:18 +0200

    Swapped my odd DIY xrandr script for the clarity of "mons -e right"

--- dot_config/bspwm/executable_hdmi_plugin.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-FILENAME=/tmp/xrandr-init.sh
-xrandr -q | grep -A1 -w connected | awk '
-    BEGIN{printf "xrandr --output eDP-1-1 --primary --mode 1920x1080 \\\n"}
-    /HDMI/ {matched=1; printf "--output %s ", $1; next}
-    matched && /^   / {printf "--mode %s --right-of eDP-1-1 \n", $1}
-' | tee $FILENAME && sh $FILENAME && rm $FILENAME && bspc wm -r
