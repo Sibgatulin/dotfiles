bf3a5dbc82bc53be618ce8ffe1a1e3c25d0a623b
Renat Sibgatulin <renat.sibgatulin@gmail.com>
2021-10-09 20:42:59 +0200

    Removed configs which are definitively not used on manjaro sway

--- dot_config/polybar/executable_launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Launch one instance of primary bar for each connected monitor
-for m in $(xrandr --query | grep -w connected | cut -d" " -f1); do
-    MONITOR=$m polybar --reload primary &
-done
-
-echo "Polybar launched..."
