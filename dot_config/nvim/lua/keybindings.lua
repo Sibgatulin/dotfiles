e3dad618247e5e8aaf87aca7c6fc9eb4a4bf9790
Renat Sibgatulin <renat.sibgatulin@gmail.com>
2022-04-16 22:58:00 +0200

    Trying to use which-key

--- dot_config/nvim/lua/keybindings.lua
+++ dot_config/nvim/lua/keybindings.lua
@@ -5,7 +5,6 @@ vim.g.mapleader = " "
 keymap("n", "<esc><esc>", "<cmd>nohlsearch<cr>", opts)
 keymap("v", "p", '"_dP', opts) -- keep yanked in the register after pasting
 keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
-keymap("n", "<leader>f", ":Rg<cr>", opts)
 
 -- Better window navigation
 keymap("n", "<c-h>", "<c-w>h", opts)
@@ -13,9 +12,25 @@ keymap("n", "<c-j>", "<c-w>j", opts)
 keymap("n", "<c-k>", "<c-w>k", opts)
 keymap("n", "<c-l>", "<c-w>l", opts)
 
-
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- More leader based mappings
+local wk = require("which-key")
+
+wk.register({
+  f = {
+    name = "fzf",
+    r = { "<cmd>Rg<cr>", "Rg" },
+    b = { "<cmd>Buffers<cr>", "Buffers" },
+  },
+  t = {
+    name = "truble",
+    w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
+    d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
+    t = { "<cmd>TroubleToggle<cr>", "toggle" },
+  },
+}, { prefix = "<leader>" })
