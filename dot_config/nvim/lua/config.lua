8c3043cf3d92c95a1ebe79835ade1f0b672eb688
Renat Sibgatulin <renat.sibgatulin@gmail.com>
2022-04-18 13:46:00 +0200

    Added mouse support and better search

--- dot_config/nvim/lua/config.lua
+++ dot_config/nvim/lua/config.lua
@@ -7,7 +7,7 @@ vim.g.backupdir = "~/.config/nvim/.backup/"
 vim.g.python3_host_prog = "/usr/bin/python"
 
 vim.g.termguicolors = true
-vim.cmd "colorscheme gruvbox-material"
+vim.cmd [[colorscheme gruvbox-material]]
 
 vim.o.expandtab = true
 vim.o.tabstop = 2
@@ -21,6 +21,13 @@ vim.wo.cursorline = true
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.o.scrolloff = 5
+
+-- Enable mouse mode
+vim.o.mouse = "a"
+vim.o.breakindent = true -- Every wrapped line will continue visually indented
+--Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
 -- By default title is off. Needed for detecting window as neovim instance (sworkstyle)
 -- but still doesn't help
