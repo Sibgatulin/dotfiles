9bf1ff3ae3a7580a4ba4cac235d3c23f0846d8d3
Renat Sibgatulin <renat.sibgatulin@gmail.com>
2022-06-23 11:55:42 +0200

    Updated null-ls to work with 0.8
    Had an issue with client.resolved_capabilities being obsolete. Current
    solution is taken from the wiki: https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save
    Additionally, black just started working with null-ls. Right?..

--- dot_config/nvim/lua/null_ls.lua
+++ dot_config/nvim/lua/null_ls.lua
@@ -9,22 +9,28 @@ local sources = {
   formatting.fixjson,
   formatting.isort,
   formatting.shfmt,
+  formatting.black, -- did it just start working well?
 
   -- diagnostics.markdownlint,
 }
 -- formatting.black, -- cannot make it work in a virtual environment
 
-nls.setup {
-  sources = sources,
-  on_attach = function(client)
-    if client.resolved_capabilities.document_formatting then
-      vim.cmd [[
-              augroup LspFormatting
-                  autocmd! * <buffer>
-                  autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
-              augroup END
-              ]]
-    end
-  end,
-  -- log = { enable = ture, level = "debug" },
-}
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+require("null-ls").setup({
+    -- you can reuse a shared lspconfig on_attach callback here
+    sources = sources,
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    -- on 0.8, you should use
+                    vim.lsp.buf.format({ bufnr = bufnr })
+                    -- vim.lsp.buf.formatting_sync()
+                end,
+            })
+        end
+    end,
+})
