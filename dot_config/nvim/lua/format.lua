cff153323f8265da3e360428c6a706b8edec649c
Renat Sibgatulin <renat.sibgatulin@gmail.com>
2022-09-22 21:16:02 +0200

    Make stylua indent with 2 spaces

--- dot_config/nvim/lua/format.lua
+++ dot_config/nvim/lua/format.lua
@@ -1,22 +1,44 @@
 -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
-require("formatter").setup {
+local util = require("formatter.util")
+require("formatter").setup({
   filetype = {
     -- Formatter configurations for filetype "lua" go here
     -- and will be executed in order
     lua = {
-      require("formatter.filetypes.lua").stylua,
+      -- require("formatter.filetypes.lua").stylua,
+      function()
+        -- Full specification of configurations is down below and in Vim help files
+        return {
+          exe = "stylua",
+          args = {
+            "--indent-type",
+            "Spaces",
+            "--indent-width",
+            "2",
+            "--search-parent-directories",
+            "--stdin-filepath",
+            util.escape_path(util.get_current_buffer_file_path()),
+            "--",
+            "-",
+          },
+          stdin = true,
+        }
+      end,
     },
 
     python = {
       require("formatter.filetypes.python").black,
       require("formatter.filetypes.python").isort,
     },
+    toml = {
+      require("formatter.filetypes.toml").taplo,
+    },
 
     -- Use the special "*" filetype for defining formatter configurations on
     -- any filetype
     ["*"] = {
       -- "formatter.filetypes.any" defines default configurations for any filetype
-      require("formatter.filetypes.any").remove_trailing_whitespace
-    }
-  }
-}
+      require("formatter.filetypes.any").remove_trailing_whitespace,
+    },
+  },
+})
