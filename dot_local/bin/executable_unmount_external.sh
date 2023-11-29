974a777e5cd9a2b93c12fb6cc1f9c3acddb680d4
Renat Sibgatulin <renat.sibgatulin@gmail.com>
2023-11-29 12:23:31 +0100

    Format bash

--- dot_local/bin/executable_unmount_external.sh
+++ dot_local/bin/executable_unmount_external.sh
@@ -1,36 +1,37 @@
 #/bin/bash
 # follow this https://askubuntu.com/questions/214646/how-to-configure-the-default-automount-location/276670#276670 to configure the automount location
 query_vendor() {
-    VENDOR=$1
-    ID=$(lsblk -S | awk -v vendor=$VENDOR '$4 == vendor  {print $1}')
-    echo $ID
+	VENDOR=$1
+	ID=$(lsblk -S | awk -v vendor=$VENDOR '$4 == vendor  {print $1}')
+	echo $ID
 }
 query_usb() {
-    ID=$(lsblk -S | awk '/usb/  {print $1}')
-    echo $ID
+	ID=$(lsblk -S | awk '/usb/  {print $1}')
+	echo $ID
 }
 unmount_by_name() {
-    ID=$1
-    BLOCK="/dev/${ID}1"
-    udisksctl unmount -b $BLOCK
-    udisksctl power-off -b $BLOCK
+	ID=$1
+	BLOCK="/dev/${ID}1"
+	udisksctl unmount -b $BLOCK
+	# check if it is a mapped device and do cryptsetup close
+	udisksctl power-off -b $BLOCK
 }
 unmount_vendor() {
-    VENDOR=$1
-    ID=$(query_vendor $VENDOR)
-    unmount_by_name $ID
+	VENDOR=$1
+	ID=$(query_vendor $VENDOR)
+	unmount_by_name $ID
 }
 unmount_all() {
-    for ID in $(query_usb); do
-        unmount_by_name $ID
-    done
+	for ID in $(query_usb); do
+		unmount_by_name $ID
+	done
 }
 
 case "$1" in
-    --vend)
-        unmount_vendor $2
-        ;;
-    --all)
-        unmount_all
-        ;;
+--vend)
+	unmount_vendor $2
+	;;
+--all)
+	unmount_all
+	;;
 esac
