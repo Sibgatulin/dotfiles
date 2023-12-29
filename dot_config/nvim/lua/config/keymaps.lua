290468003866bc20bb03ea8d066b204274b16035
Renat Sibgatulin <renat.sibgatulin@gmail.com>
2023-12-29 13:53:58 +0100

    A very hacky way to add JSON5 formatting
    TODO:
    1. use conform.nvim or none-ls
    2. only register the keymap " c5" in .json5 files

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.api.nvim_create_user_command("YankFullPath", function()
-  local path = vim.fn.expand("%:p")
+  local path = vim.fn.expand "%:p"
   vim.fn.setreg("+", path)
   vim.notify('Copied "' .. path .. '" to the clipboard')
 end, {})
@@ -12,4 +12,19 @@ vim.keymap.set(
   "<leader>fy",
   [[<cmd>YankFullPath<cr>]],
   { desc = "Yank current buffer's full path to the clipboard" }
+)
+
+vim.api.nvim_create_user_command("FormatJSON5", function()
+  local path = vim.fn.expand "%:p"
+  local command = "/data/soft/json5format/target/debug/examples/formatjson5 -r " .. path
+  vim.fn.system(command)
+  vim.api.nvim_command "edit"
+  vim.notify('Formatted "' .. path .. '"')
+end, {})
+
+vim.keymap.set(
+  "n",
+  "<leader>c5",
+  [[<cmd>FormatJSON5<cr>]],
+  { desc = "Format current JSON5" }
 )
